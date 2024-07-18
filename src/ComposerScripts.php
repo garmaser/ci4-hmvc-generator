10ae51e9ea95990110812ccd89099cfafd35fb52
garmaser <sergiogarciamamani@gmail.com>
2024-07-18 17:14:07 -0400

    upedate ComposerScript.php

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -1,39 +1,32 @@
-<?php
-
-namespace garmaser\Ci4HmvcGenerator;
-
-use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
-
-class ComposerScripts
+private static function copyCommands(Event $event)
 {
-    public static function postInstall(Event $event)
-    {
-        self::copyCommands($event);
+    $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+    $baseDir = dirname($vendorDir);
+    
+    $sourceDir = $baseDir . '/vendor/garmaser/ci4-hmvc-generator/src/Commands/Hmvc';
+    $targetDir = $baseDir . '/app/Commands/Hmvc';
+    
+    $event->getIO()->write('Source directory: ' . $sourceDir);
+    $event->getIO()->write('Target directory: ' . $targetDir);
+    
+    if (!is_dir($sourceDir)) {
+        $event->getIO()->write('Source directory does not exist!');
+        return;
     }
-
-    public static function postUpdate(Event $event)
-    {
-        self::copyCommands($event);
+    
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0755, true);
     }
-
-    private static function copyCommands(Event $event)
-    {
-        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        $baseDir = dirname($vendorDir);
-
-        $sourceDir = $baseDir . '/vendor/garmaser/ci4-hmvc-generator/src/Commands/Hmvc';
-        $targetDir = $baseDir . '/app/Commands/Hmvc';
-
-        if (!is_dir($targetDir)) {
-            mkdir($targetDir, 0755, true);
+    
+    foreach (glob($sourceDir . '/*.php') as $file) {
+        $filename = basename($file);
+        $event->getIO()->write('Copying file: ' . $filename);
+        if (copy($file, $targetDir . '/' . $filename)) {
+            $event->getIO()->write('Successfully copied: ' . $filename);
+        } else {
+            $event->getIO()->write('Failed to copy: ' . $filename);
         }
-
-        foreach (glob($sourceDir . '/*.php') as $file) {
-            $filename = basename($file);
-            copy($file, $targetDir . '/' . $filename);
-        }
-
-        $event->getIO()->write('HMVC commands have been copied to ' . $targetDir);
     }
+    
+    $event->getIO()->write('HMVC commands have been copied to ' . $targetDir);
 }
