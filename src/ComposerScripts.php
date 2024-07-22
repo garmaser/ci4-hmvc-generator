4b1011e62e32df368a9394dbf853a6d977070612
garmaser <sergiogarciamamani@gmail.com>
2024-07-22 12:29:56 -0400

    feat: Implement automatic HMVC command installation - Add ComposerScripts for post-install and post-update hooks - Improve command copying process with color-coded feedback - Update README with new installation instructions

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -1,32 +1,39 @@
-private static function copyCommands(Event $event)
+<?php
+
+namespace garmaser\Ci4HmvcGenerator;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class ComposerScripts
 {
-    $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-    $baseDir = dirname($vendorDir);
-    
-    $sourceDir = $baseDir . '/vendor/garmaser/ci4-hmvc-generator/src/Commands/Hmvc';
-    $targetDir = $baseDir . '/app/Commands/Hmvc';
-    
-    $event->getIO()->write('Source directory: ' . $sourceDir);
-    $event->getIO()->write('Target directory: ' . $targetDir);
-    
-    if (!is_dir($sourceDir)) {
-        $event->getIO()->write('Source directory does not exist!');
-        return;
+    public static function postInstall(Event $event)
+    {
+        self::copyCommands($event);
     }
-    
-    if (!is_dir($targetDir)) {
-        mkdir($targetDir, 0755, true);
+
+    public static function postUpdate(Event $event)
+    {
+        self::copyCommands($event);
     }
-    
-    foreach (glob($sourceDir . '/*.php') as $file) {
-        $filename = basename($file);
-        $event->getIO()->write('Copying file: ' . $filename);
-        if (copy($file, $targetDir . '/' . $filename)) {
-            $event->getIO()->write('Successfully copied: ' . $filename);
-        } else {
-            $event->getIO()->write('Failed to copy: ' . $filename);
+
+    public static function copyCommands(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $baseDir = dirname($vendorDir);
+
+        $sourceDir = $baseDir . '/vendor/garmaser/ci4-hmvc-generator/src/Commands/Hmvc';
+        $targetDir = $baseDir . '/app/Commands/Hmvc';
+
+        if (!is_dir($targetDir)) {
+            mkdir($targetDir, 0755, true);
         }
+
+        foreach (glob($sourceDir . '/*.php') as $file) {
+            $filename = basename($file);
+            copy($file, $targetDir . '/' . $filename);
+        }
+
+        $event->getIO()->write('<info>HMVC commands have been copied to ' . $targetDir.'</info>');
     }
-    
-    $event->getIO()->write('HMVC commands have been copied to ' . $targetDir);
 }
